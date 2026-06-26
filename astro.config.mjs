c9fa6381c7bad9606e0f8af877505656ac973e6a
manuelgerstner <manuelgerstner@gmail.com>
2026-06-26 18:36:15 +0200

    Fix dependency issue

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,7 @@
 import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
   site: 'https://manuelgerstner.github.io',
-  integrations: [tailwind()],
   i18n: {
     defaultLocale: 'en',
     locales: ['en', 'de', 'pt', 'fr'],
